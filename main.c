eb50dd00cb0650c936d8b5426dc236eb73c81a0f
Diego Tsuyoshi <dts2896@gmail.com>
2018-05-22 09:44:17 -0400

    Testando funções de arquivo

--- main.c
+++ main.c
@@ -1,10 +1,19 @@
 #include <stdio.h>
 #include "lib/arquivo.h"
 
-#define T 7
 
 int main(int argc, char const *argv[]) {	
 	
-	
+	int matriz_adjacencia[MAX_T][MAX_T];
+	int i, j;
+
+	ler_arquivo(abrir_arquivo("Rede.txt", "r"), matriz_adjacencia);
+
+
+	for(i = 1; i <= T; i++) {
+		for(j = 1; j <= T; j++)
+			printf("%d ", matriz_adjacencia[i][j]);
+		printf("\n");
+	}
 	return 0;
 }

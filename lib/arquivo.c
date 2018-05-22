5ce6a838c2091135012a90c593d3846c58ecfb92
Diego Tsuyoshi <dts2896@gmail.com>
2018-05-22 09:29:09 -0400

    Corrigindo cabeçalhos

---
+++ lib/arquivo.c
@@ -0,0 +1,39 @@
+#include "arquivo.h"
+
+/*
+Parâmetros: 
+	char * nome = Nome do arquivo
+	char * modo = Modo em que o arquivo deve ser aberto. "r, w, rw,..."
+
+Retorno:
+	Arquivo ou Nulo (caso falha)
+*/
+FILE * abrir_arquivo(char * nome, char * modo) {
+
+	FILE* arquivo = fopen(nome, modo);
+
+	if (!arquivo) { 
+
+		printf("Ocorreu um erro ao abrir o arquivo!\n");
+
+		return NULL;
+	}
+	else
+		return arquivo;
+}
+
+/*
+Parâmetros:
+	FILE * arquivo = Arquivo aberto
+	int vetor[] = Vetor que irá receber a entrada contida no arquivo
+*/
+void ler_arquivo(FILE * arquivo, int matriz_adjacencia[][T]) {
+
+	int i, j;
+
+	// Realizando a leitura do arquivo e gravando na matriz de adjacencia
+	for(i = 1; i <= T; i++)
+		for(j = 1; j <= T; j++) 
+			fscanf(arquivo, "%d", &matriz_adjacencia[i][j]);
+		
+}
--- lib/arquivo.h
+++ lib/arquivo.h
@@ -1,39 +1,11 @@
+
+#ifndef _ARQUIVO_H
+
 #define T 7
 
-/*
-Parâmetros: 
-	char * nome = Nome do arquivo
-	char * modo = Modo em que o arquivo deve ser aberto. "r, w, rw,..."
 
-Retorno:
-	Arquivo ou Nulo (caso falha)
-*/
-FILE * abrir_arquivo(char * nome, char * modo) {
+FILE * abrir_arquivo(char * nome, char * modo);
+void ler_arquivo(FILE * arquivo, int matriz_adjacencia[][T]);
 
-	FILE* arquivo = fopen(nome, modo);
+#endif
 
-	if (!arquivo) { 
-
-		printf("Ocorreu um erro ao abrir o arquivo!\n");
-
-		return NULL;
-	}
-	else
-		return arquivo;
-}
-
-/*
-Parâmetros:
-	FILE * arquivo = Arquivo aberto
-	int vetor[] = Vetor que irá receber a entrada contida no arquivo
-*/
-void ler_arquivo(FILE * arquivo, int matriz_adjacencia[][T]) {
-
-	int i, j;
-
-	// Realizando a leitura do arquivo e gravando na matriz de adjacencia
-	for(i = 1; i <= T; i++)
-		for(j = 1; j <= T; j++) 
-			fscanf(arquivo, "%d", &matriz_adjacencia[i][j]);
-		
-}

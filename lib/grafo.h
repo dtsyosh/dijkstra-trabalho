c8bcf8bdc09692380205dc3dc6166cc4cb14c901
Diego Tsuyoshi <dts2896@gmail.com>
2018-05-24 09:48:32 -0400

    Adicionando o algoritmo de dijkstra

---
+++ lib/grafo.h
@@ -0,0 +1,10 @@
+#ifndef _GRAFO_H_
+
+#include <stdio.h>
+
+#define MAX_T 10
+#define T 6
+
+void dijkstra(int matriz[][MAX_T], int distancia[], int quantidadeVertices, int verticeInicial, int verticeFinal);
+
+#endif

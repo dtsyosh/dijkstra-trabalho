9849b75a9572bdba5e5582de71f0e5648cc037c5
Diego Tsuyoshi <dts2896@gmail.com>
2018-05-24 09:48:43 -0400

    Adicionando algoritmo de dijkstra

---
+++ src/grafo.c
@@ -0,0 +1,50 @@
+#include "../lib/grafo.h"
+
+void dijkstra(int matriz[][MAX_T], int distancia[], int quantidadeVertices, int verticeInicial, int verticeFinal)
+{
+	int fechaVertice[MAX_T];			//Vertices que ja foram fechados
+	int contador, menorDistancia, vertMenorDistancia, i, linha, coluna;
+	int verticeAtual;
+    int caminho[50];
+
+
+	for(linha = 1; linha <= quantidadeVertices; linha++)
+		for(coluna = 1; coluna <= quantidadeVertices; coluna++)
+			if(matriz[linha][coluna] == 0)
+				matriz[linha][coluna] = 999;
+
+
+	for(contador = 1; contador <= quantidadeVertices; contador++)
+	{
+		fechaVertice[contador] = 0;		//Estado do vertice
+		distancia[contador] = matriz[verticeInicial][contador];		//Adquirindo a distancia dos vizinhos da origem
+	}
+
+	fechaVertice[verticeInicial] = 1;
+	contador = 2;		//Começa em 2 pois o ponto de origem não entra nos calculos
+
+	while(contador <= quantidadeVertices)
+	{
+		menorDistancia = 99;
+
+		for(i = 1; i <= quantidadeVertices; i++)
+		{
+			if(distancia[i] < menorDistancia && !fechaVertice[i])			// Se a distância do vertice 'i' for menor do que o da variavel Dist e o vertice não estiver fechado
+			{
+				menorDistancia = distancia[i];		// Grava uma distancia menor
+				vertMenorDistancia = i;				//Gravo o vertice que pertencie a menor distancia gravada
+			}
+		}
+
+		fechaVertice[vertMenorDistancia] = 1;     //Fecho o vertice com a menor distancia.
+
+		for(i = 1; i <= quantidadeVertices; i++)			//Atualiza as distancias passando por este vertice
+		{
+			if( (distancia[vertMenorDistancia] + matriz[vertMenorDistancia][i]) < distancia[i] && !fechaVertice[i])	//Se a soma do custo do vertice atual + o caminho pro proximo vertice for menor que o custo do proximo vertice E ele estiver aberto
+				distancia[i] = distancia[vertMenorDistancia] + matriz[vertMenorDistancia][i];						//Atualizo o custo daquele vertice passando pelo vertice atual
+		}
+
+		contador++;
+
+	}
+}
